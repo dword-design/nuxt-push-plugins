96f3d3b54408f45e20f67e12c6ca80d8ce743c40
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-10-17 14:28:20 +0000

    fix: make nuxt 3 compatible

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import { forEach } from '@dword-design/functions'
-import P from 'path'
 
 export default (self, ...plugins) =>
   forEach(plugins, plugin => {
-    const template = self.addTemplate(plugin)
-    self.options.plugins.push({
-      mode: plugin.mode,
-      src: P.join(self.options.buildDir, template.dst),
-    })
+    self.addTemplate(plugin)
+    self.options.plugins.push(plugin)
   })

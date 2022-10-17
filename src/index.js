348d7516708f5abd6b2a5773f2f0c330454f9e57
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-10-17 14:52:52 +0000

    fix: undo last changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import { forEach } from '@dword-design/functions'
+import P from 'path'
 
 export default (self, ...plugins) =>
   forEach(plugins, plugin => {
-    self.addTemplate(plugin)
-    self.options.plugins.push(plugin)
+    const template = self.addTemplate(plugin)
+    self.options.plugins.push({
+      mode: plugin.mode,
+      src: P.join(self.options.buildDir, template.dst),
+    })
   })

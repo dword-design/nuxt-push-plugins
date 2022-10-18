59db5b945373717a60fedaf3616afb2c989082ba
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-10-18 01:00:58 +0000

    fix: fix linting

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,95 +1,49 @@
-import { delay, endent, isEqual, mapValues } from '@dword-design/functions'
-import puppeteer from '@dword-design/puppeteer'
+import { delay, endent, isEqual } from '@dword-design/functions'
+import tester from '@dword-design/tester'
+import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
+import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
 import execa from 'execa'
+import { outputFile } from 'fs-extra'
 import outputFiles from 'output-files'
 import pWaitFor from 'p-wait-for'
 import P from 'path'
-import withLocalTmpDir from 'with-local-tmp-dir'
 import kill from 'tree-kill-promise'
-import { outputFile } from 'fs-extra'
 
-let browser
-let page
-
-const runTest = config => {
-  config = { consoleMessages: [], test: () => {}, ...config }
-
-  return () =>
-    withLocalTmpDir(async () => {
-      await outputFiles({
-        'package.json': JSON.stringify({ type: 'module' }),
-        ...config.files,
-      })
-      const versions = {
-        2: 'nuxt',
-        3: 'nuxt@3.0.0-rc.11'
-      }
-      for (const version in versions) {
-        console.log(versions[version])
-        await outputFile('nuxt.config.js', `export default ${JSON.stringify({ build: { quiet: false }, ...config.nuxtConfigs[version] })}`),
-        await execa.command(`yarn add --dev ${versions[version]}`)
-        await execa(P.resolve('node_modules', '.bin', 'nuxt'), ['build'])
-        const childProcess = execa(P.resolve('node_modules', '.bin', 'nuxt'), ['start'])
-        await delay(2000)
-
-        const messages = []
-
-        const consoleHandler = message => messages.push(message.text())
-        page.on('console', consoleHandler)
-        await page.goto('http://localhost:3000')
-        if (config.consoleMessages.length > 0) {
-          await pWaitFor(() => messages |> isEqual(config.consoleMessages))
-        }
-        page.off('console', consoleHandler)
-        try {
-          await config.test()
-        } finally {
-          await kill(childProcess.pid)
-        }
-      }
-    })
-}
-
-export default {
-  after: () => browser.close(),
-  before: async () => {
-    browser = await puppeteer.launch()
-    page = await browser.newPage()
-  },
-  ...({
+export default tester(
+  {
     works: {
       consoleMessages: ['foo', 'bar'],
       files: {
         modules: {
           bar: {
             'index.js': endent`
-              import self from '../../../src'
+            import self from '../../../src'
 
-             export default function () {
-                self(this, require.resolve('./plugin'))
-              }
+            export default function () {
+              self(this, require.resolve('./plugin'))
+            }
 
-            `,
+          `,
             'plugin.js': "export default () => console.log('bar')",
           },
           foo: {
             'index.js': endent`
-              import self from '../../../src'
+            import self from '../../../src'
 
-              export default function () {
-                self(this, require.resolve('./plugin'))
-              }
+            export default function () {
+              self(this, require.resolve('./plugin'))
+            }
 
-            `,
+          `,
             'plugin.js': "export default () => console.log('foo')",
           },
         },
         'pages/index.vue': endent`
-          <template>
-            <div />
-          </template>
+        <template>
+          <div />
+        </template>
 
-        `,
+      `,
       },
       nuxtConfigs: {
         2: {
@@ -98,7 +52,61 @@ export default {
         3: {
           modules: ['./modules/foo', './modules/bar'],
         },
-      }
+      },
     },
-  }) |> mapValues(runTest),
-}
+  },
+  [
+    testerPluginTmpDir(),
+    {
+      transform: config => {
+        config = { consoleMessages: [], test: () => {}, ...config }
+
+        return async function () {
+          await outputFiles({
+            'package.json': JSON.stringify({ type: 'module' }),
+            ...config.files,
+          })
+
+          const versions = {
+            2: 'nuxt',
+            3: 'nuxt@3.0.0-rc.11',
+          }
+          for (const version of Object.keys(versions)) {
+            console.log(versions[version])
+            await outputFile(
+              'nuxt.config.js',
+              `export default ${JSON.stringify({
+                build: { quiet: false },
+                ...config.nuxtConfigs[version],
+              })}`
+            )
+            await execa.command(`yarn add --dev ${versions[version]}`)
+            await execa(P.resolve('node_modules', '.bin', 'nuxt'), ['build'])
+
+            const childProcess = execa(
+              P.resolve('node_modules', '.bin', 'nuxt'),
+              ['start']
+            )
+            await delay(2000)
+
+            const messages = []
+
+            const consoleHandler = message => messages.push(message.text())
+            this.page.on('console', consoleHandler)
+            await this.page.goto('http://localhost:3000')
+            if (config.consoleMessages.length > 0) {
+              await pWaitFor(() => messages |> isEqual(config.consoleMessages))
+            }
+            this.page.off('console', consoleHandler)
+            try {
+              await config.test()
+            } finally {
+              await kill(childProcess.pid)
+            }
+          }
+        }
+      },
+    },
+    testerPluginPuppeteer(),
+  ]
+)

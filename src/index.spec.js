6d6139dec46f571a61e8fa5665669bc29774826e
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-11-03 14:57:51 +0000

    fix: simplify tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,16 +1,61 @@
-import { delay, endent, identity, mapValues } from '@dword-design/functions'
+import { delay, endent } from '@dword-design/functions'
 import tester from '@dword-design/tester'
 import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
 import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
 import packageName from 'depcheck-package-name'
 import execa from 'execa'
-import { outputFile, remove, symlink } from 'fs-extra'
+import { outputFile } from 'fs-extra'
+import { Builder, Nuxt } from 'nuxt'
 import outputFiles from 'output-files'
 import P from 'path'
 import kill from 'tree-kill-promise'
 
 export default tester(
   {
+    nuxt3: {
+      files: {
+        modules: {
+          bar: {
+            'index.js': endent`
+            import self from '../../../src'
+
+            export default function () {
+              self(this, require.resolve('./plugin'))
+            }
+
+          `,
+            'plugin.js':
+              "export default (context, inject) => inject('bar', context.$foo)",
+          },
+          foo: {
+            'index.js': endent`
+            import self from '../../../src'
+
+            export default function () {
+              self(this, require.resolve('./plugin'))
+            }
+
+          `,
+            'plugin.js':
+              "export default (context, inject) => inject('foo', 'bar')",
+          },
+        },
+        'pages/index.vue': endent`
+        <template>
+          <div :class="$bar" />
+        </template>
+
+      `,
+      },
+      nuxtConfig: {
+        modules: ['./modules/foo', './modules/bar'],
+      },
+      nuxtVersion: 3,
+      async test() {
+        await this.page.goto('http://localhost:3000')
+        await this.page.waitForSelector('.bar')
+      },
+    },
     works: {
       files: {
         modules: {
@@ -51,7 +96,6 @@ export default tester(
       },
       async test() {
         await this.page.goto('http://localhost:3000')
-        console.log(await this.page.content())
         await this.page.waitForSelector('.bar')
       },
     },
@@ -59,57 +103,21 @@ export default tester(
   [
     testerPluginTmpDir(),
     {
-      after: () => Promise.all([remove('nuxt2'), remove('nuxt3')]),
-      before: async () => {
-        await outputFile(P.join('nuxt2', 'package.json'), JSON.stringify({}))
-        await execa.command('yarn add --dev nuxt', { cwd: 'nuxt2' })
-        await outputFile(P.join('nuxt3', 'package.json'), JSON.stringify({}))
-        await execa.command('yarn add --dev nuxt@3.0.0-rc.12', {
-          cwd: 'nuxt3',
-        })
-      },
       transform: config => {
-        config = { test: () => {}, ...config }
+        config = { nuxtConfig: {}, nuxtVersion: 2, test: () => {}, ...config }
 
         return async function () {
           await outputFiles({
             'package.json': JSON.stringify({ type: 'module' }),
             ...config.files,
           })
-          let versions = {
-            2: {},
-            3: {
-              transformNuxtConfig: nuxtConfig =>
-                nuxtConfig
-                |> mapValues((value, key) =>
-                  key === 'modules'
-                    ? value.map(mod =>
-                        mod.startsWith('~') ? `.${mod.slice(1)}` : mod
-                      )
-                    : value
-                ),
-            },
-          }
-          versions =
-            versions
-            |> mapValues(versionConfig => ({
-              transformNuxtConfig: identity,
-              ...versionConfig,
-            }))
-          for (const version of Object.keys(versions)) {
-            console.log(version)
-            await remove('node_modules')
-            await symlink(
-              P.join('..', `nuxt${version}`, 'node_modules'),
-              'node_modules'
-            )
+          if (config.nuxtVersion === 3) {
+            await execa.command('yarn add --dev nuxt@3.0.0-rc.12')
             await outputFile(
               'nuxt.config.js',
               `export default ${JSON.stringify({
-                build: { quiet: false },
-                rootDir: process.cwd(),
                 telemetry: false,
-                ...versions[version].transformNuxtConfig(config.nuxtConfig),
+                ...config.nuxtConfig,
               })}`
             )
 
@@ -118,16 +126,32 @@ export default tester(
               '.bin',
               packageName`nuxt`
             )
-            await execa(nuxtPath, ['build'], { stdio: 'inherit' })
+            await execa(nuxtPath, ['build'])
 
-            const childProcess = execa(nuxtPath, ['start'], {
-              stdio: 'inherit',
-            })
+            const childProcess = execa(nuxtPath, ['start'])
             await delay(5000)
             try {
               await config.test.call(this)
             } finally {
               await kill(childProcess.pid)
+            }
+          } else {
+            const nuxt = new Nuxt({
+              dev: false,
+              ...config.nuxtConfig,
+            })
+            if (config.error) {
+              await expect(new Builder(nuxt).build()).rejects.toThrow(
+                config.error
+              )
+            } else {
+              await new Builder(nuxt).build()
+              await nuxt.listen()
+              try {
+                await config.test.call(this)
+              } finally {
+                await nuxt.close()
+              }
             }
           }
         }

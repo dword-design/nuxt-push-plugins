5f14c25f648bbdf201980757c5c85367ed0e78fd
Sebastian Landwehr <info@sebastianlandwehr.com>
2022-11-03 11:09:50 +0000

    fix: foo

--- src/index.spec.js
+++ src/index.spec.js
@@ -2,8 +2,9 @@ import { delay, endent, identity, mapValues } from '@dword-design/functions'
 import tester from '@dword-design/tester'
 import testerPluginPuppeteer from '@dword-design/tester-plugin-puppeteer'
 import testerPluginTmpDir from '@dword-design/tester-plugin-tmp-dir'
+import packageName from 'depcheck-package-name'
 import execa from 'execa'
-import { outputFile } from 'fs-extra'
+import { outputFile, remove, symlink } from 'fs-extra'
 import outputFiles from 'output-files'
 import P from 'path'
 import kill from 'tree-kill-promise'
@@ -58,6 +59,26 @@ export default tester(
   [
     testerPluginTmpDir(),
     {
+      after: () => Promise.all([remove('nuxt2'), remove('nuxt3')]),
+      before: () =>
+        Promise.all([
+          (async () => {
+            await outputFile(
+              P.join('nuxt2', 'package.json'),
+              JSON.stringify({})
+            )
+            await execa.command('yarn add --dev nuxt', { cwd: 'nuxt2' })
+          })(),
+          (async () => {
+            await outputFile(
+              P.join('nuxt3', 'package.json'),
+              JSON.stringify({})
+            )
+            await execa.command('yarn add --dev nuxt@3.0.0-rc.11', {
+              cwd: 'nuxt3',
+            })
+          })(),
+        ]),
       transform: config => {
         config = { test: () => {}, ...config }
 
@@ -67,7 +88,8 @@ export default tester(
             ...config.files,
           })
           let versions = {
-            'nuxt@3.0.0-rc.11': {
+            2: {},
+            3: {
               transformNuxtConfig: nuxtConfig =>
                 nuxtConfig
                 |> mapValues((value, key) =>
@@ -78,7 +100,6 @@ export default tester(
                     : value
                 ),
             },
-            'nuxt@^2': {},
           }
           versions =
             versions
@@ -88,21 +109,31 @@ export default tester(
             }))
           for (const version of Object.keys(versions)) {
             console.log(version)
+            await remove('node_modules')
+            await symlink(
+              P.join('..', `nuxt${version}`, 'node_modules'),
+              'node_modules'
+            )
             await outputFile(
               'nuxt.config.js',
               `export default ${JSON.stringify({
                 build: { quiet: false },
+                rootDir: process.cwd(),
+                telemetry: false,
                 ...versions[version].transformNuxtConfig(config.nuxtConfig),
               })}`
             )
-            await execa.command(`yarn add --dev ${version}`)
-            await execa(P.resolve('node_modules', '.bin', 'nuxt'), ['build'], { stdio: 'inherit' })
 
-            const childProcess = execa(
-              P.resolve('node_modules', '.bin', 'nuxt'),
-              ['start'],
-              { stdio: 'inherit' }
+            const nuxtPath = P.resolve(
+              'node_modules',
+              '.bin',
+              packageName`nuxt`
             )
+            await execa(nuxtPath, ['build'], { stdio: 'inherit' })
+
+            const childProcess = execa(nuxtPath, ['start'], {
+              stdio: 'inherit',
+            })
             await delay(5000)
             try {
               await config.test.call(this)
